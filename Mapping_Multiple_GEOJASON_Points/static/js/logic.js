02c981416ab4248fd558ee48111c4f2fe2bbd4f5
Joe Goeman <jgoeman86@gmail.com>
2022-01-01 21:02:57 -0600

    Mapping multiple using majorAirports

---
+++ Mapping_Multiple_GEOJASON_Points/static/js/logic.js
@@ -0,0 +1,41 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+//let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Create the map object with center and zoom level.
+let map = L.map("mapid", {
+center: [
+    30, 30
+],
+    zoom: 2
+});
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/jgoeman/Mapping_Earthquakes/main/majorAirports.json"
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+      onEachFeature: function(feature,layer) {
+        console.log(feature)
+        layer.bindPopup("<h2> Airport code: "  + feature.properties.faa + "<hr></hr>" + "<h3> Airport name: "+ feature.properties.name +"</h3>" + "</h2>")
+      }
+  })
+  
+  .addTo(map);
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
